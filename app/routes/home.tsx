9a637699f1ea9e866c22c2e4dbdf795b0f75bf1b
JodiAB <jodiabrahams9@gmail.com>
2025-07-21 15:42:37 +0200

    Home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,39 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CareerCrafter" },
+    {
+      name: "description",
+      content:
+        "AI-powered resume analysis to optimize your professional profile and boost job application success.",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+
+      {Array.isArray(resumes) && resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+            </div>
+     
+      )}
+      </section>
+    </main>
+  );
 }
