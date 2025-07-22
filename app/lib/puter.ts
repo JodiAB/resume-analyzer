67fe40fcf19441b7d1074df2ad2cae98145bcef4
JodiAB <jodiabrahams9@gmail.com>
2025-07-22 12:16:53 +0200

    upload fixed

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -350,7 +350,7 @@ export const usePuterStore = create<PuterStore>((set, get) => {
                     ],
                 },
             ],
-            { model: "claude-3-7-sonnet" }
+            { model: "grok-3" }
         ) as Promise<AIResponse | undefined>;
     };
 
